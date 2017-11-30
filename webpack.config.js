081550f7715dd0d44fd854f146038df77bad0110
Mike Antoniadis <ssbeefeater@gmail.com>
2017-11-30 18:12:12 +0200

    add external in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,20 @@ const config = ({
         './example/index.js',
     ],
     devtool: 'source-map',
+    externals: {
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react',
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom',
+        },
+    },
     module: {
         rules: [
             {
