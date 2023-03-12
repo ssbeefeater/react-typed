f3a24c0bdc177556841cab5d486c0cdaf218efc9
Laurent Meyer <94935904+laurentmmeyer@users.noreply.github.com>
2023-03-12 23:13:13 +0100

    Update react-typed.d.ts

--- react-typed.d.ts
+++ react-typed.d.ts
@@ -63,6 +63,7 @@ declare module 'react-typed' {
         onDestroy?: Function,
         className?: string,
         style?: React.CSSProperties,
+        children: Element
     }
     class ReactTyped extends React.Component<ReactTypedProps, any> { }
     export default ReactTyped
